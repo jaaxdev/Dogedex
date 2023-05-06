8badb7981e8e09500b074c33d491da9d66391158
jaaxdev <jaaziel.irc@gmail.com>
2023-05-06 11:31:33 -0700

    manejo de errores 1

--- app/src/main/java/com/jaax/dogedex/doglist/DogListViewModel.kt
+++ app/src/main/java/com/jaax/dogedex/doglist/DogListViewModel.kt
@@ -4,11 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jaax.dogedex.api.ApiResponsesStatus
 import kotlinx.coroutines.launch
 
-class DogListViewModel: ViewModel() {
+class DogListViewModel : ViewModel() {
     private var _dogList = MutableLiveData<List<Dog>>()
     val dogList: LiveData<List<Dog>> get() = _dogList
+
+    private var _status = MutableLiveData<ApiResponsesStatus>()
+    val status: LiveData<ApiResponsesStatus> get() = _status
 
     private val dogRepository = DogRepository()
 
@@ -18,7 +22,15 @@ class DogListViewModel: ViewModel() {
 
     private fun downloadDogs() {
         viewModelScope.launch {
-            _dogList.value = dogRepository.downloadDogs()
+            _status.value = ApiResponsesStatus.LOADING
+
+            try {
+                _dogList.value = dogRepository.downloadDogs()
+                _status.value = ApiResponsesStatus.SUCCESS
+            } catch(e: Exception) {
+                _status.value = ApiResponsesStatus.ERROR
+            }
+
         }
     }
 }
